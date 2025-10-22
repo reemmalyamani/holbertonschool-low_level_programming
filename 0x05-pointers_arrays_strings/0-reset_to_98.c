147bcfc948caac17d5a0c81ee82ec7443506b690
Reem Alyamani <your_github_email@example.com>
2025-10-22 10:31:34 +0000

    Fresh rebuild of 0x05-pointers_arrays_strings task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,6 @@
 #include "main.h"
-
 /**
- * reset_to_98 - takes a pointer to an int and updates the value it points to to 98
- * @n: pointer to the integer to be updated
- *
- * Return: void
+ * reset_to_98 - takes a pointer to an int and updates the value to 98
+ * @n: pointer to int
  */
-void reset_to_98(int *n)
-{
-	*n = 98;
-}
+void reset_to_98(int *n){*n=98;}
