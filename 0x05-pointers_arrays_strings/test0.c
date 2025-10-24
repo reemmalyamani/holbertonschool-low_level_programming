53acc9fe368b1b05d6bc76c0b42662193bfdd80a
reemmalyamani <alyamanireem@gmail.com>
2025-10-24 07:59:50 +0000

    Final verified version: 0-reset_to_98 and 1-swap

---
+++ 0x05-pointers_arrays_strings/test0.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+int main(void)
+{
+    int n = 402;
+    printf("n=%d\n", n);
+    reset_to_98(&n);
+    printf("n=%d\n", n);
+    return (0);
+}
---
+++ 0x05-pointers_arrays_strings/test1.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+int main(void)
+{
+    int a = 98, b = 42;
+    printf("a=%d, b=%d\n", a, b);
+    swap_int(&a, &b);
+    printf("a=%d, b=%d\n", a, b);
+    return (0);
+}
