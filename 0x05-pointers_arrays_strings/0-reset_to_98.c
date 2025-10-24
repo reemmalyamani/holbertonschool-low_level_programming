a875a9a65c1594b7c6758f8c264aff5e33c2f7f1
reemmalyamani <alyamanireem@gmail.com>
2025-10-24 08:19:07 +0000

    Final rebuild: clean folder for checker

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int and updates the value it points to to 98
- * @n: pointer to an integer
+ * reset_to_98 - updates the value pointed to by n to 98
+ * @n: pointer to an int
  */
 void reset_to_98(int *n)
 {
-	*n = 98;
+    *n = 98;
 }
