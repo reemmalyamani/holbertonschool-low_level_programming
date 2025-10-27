da98f392fd251448ef00b98e4cdaf5f5b8a8d663
reemmalyamani <alyamanireem@gmail.com>
2025-10-27 12:32:24 +0000

    Add _strchr function

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -22,5 +22,6 @@ int _strcmp(char *s1, char *s2);
 char *leet(char *str);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif /* MAIN_H */
