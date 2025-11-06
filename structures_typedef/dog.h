83c09a35832a28946bf73631cf5556a4ef66afd2
reemmalyamani <alyamanireem@gmail.com>
2025-11-06 14:03:17 +0000

    Add init_dog function

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,6 +9,25 @@
  */
 struct dog
 {
+    char *name;
+    float age;
+    char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif /* DOG_H */
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
