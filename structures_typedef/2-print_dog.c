d1a5fc2f4762f35e30b54d867744e15a9ef7fb0b
reemmalyamani <alyamanireem@gmail.com>
2025-11-06 14:31:27 +0000

    Recreate print_dog function clean

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,6 +1,5 @@
 #include "dog.h"
 #include <stdio.h>
-#include <stddef.h>
 
 /**
  * print_dog - prints a struct dog
@@ -8,18 +7,18 @@
  */
 void print_dog(struct dog *d)
 {
-    if (d == NULL)
-        return;
+	if (d == NULL)
+		return;
 
-    if (d->name == NULL)
-        printf("Name: (nil)\n");
-    else
-        printf("Name: %s\n", d->name);
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
 
-    printf("Age: %.6f\n", d->age);
+	printf("Age: %f\n", d->age);
 
-    if (d->owner == NULL)
-        printf("Owner: (nil)\n");
-    else
-        printf("Owner: %s\n", d->owner);
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
