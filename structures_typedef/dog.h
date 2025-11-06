a7f22f1e4c851366246efba0102c5962a1a76dc0
reemmalyamani <alyamanireem@gmail.com>
2025-11-06 14:08:21 +0000

    Fix dog struct and init_dog function

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,24 @@
+#
 #ifndef DOG_H
 #define DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Dog info
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif#define DOG_H
 
 /**
  * struct dog - information about a dog
