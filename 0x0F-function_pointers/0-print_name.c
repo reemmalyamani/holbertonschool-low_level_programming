c49107b32c740535989bb3444339a3709c0b5271
Reem Alyamani <YOUR_EMAIL@example.com>
2025-11-10 15:15:47 +0000

    Remove inline comment and fix final Betty style

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,10 +4,11 @@
  * print_name - prints a name using a callback
  * @name: string to print
  * @f: function pointer that prints a string
+ *
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	/* Betty: use tabs, not spaces */
 	if (name == NULL || f == NULL)
 		return;
 
