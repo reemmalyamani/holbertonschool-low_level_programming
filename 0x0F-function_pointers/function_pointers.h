a75caa303fd186164454147aa5869f76f9dfe06c
reemmalyamani <alyamanireem@gmail.com>
2025-11-10 15:08:46 +0000

    Fix Betty style in function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,13 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stddef.h>  /* Needed for size_t and NULL */
+#include <stddef.h>
 
+/**
+ * print_name - prints a name
+ * @name: the name to be printed
+ * @f: pointer to a function that prints a name
+ */
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
 
-#endif
+#endif /* FUNCTION_POINTERS_H */
