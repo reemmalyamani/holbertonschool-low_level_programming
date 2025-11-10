3d3e7b72d4f6208679eedc1f3e9a292e09c5f496
reemmalyamani <alyamanireem@gmail.com>
2025-11-10 15:07:01 +0000

    Fix Betty style for print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
 /**
-	* print_name - prints a name using a function given
-	* @name: the name to print
-	* @f: pointer to a function that takes a char * and returns void
-	*
-	* Return: Nothing.
-	*/
+ * print_name - prints a name using a function given
+ * @name: the name to print
+ * @f: pointer to a function that takes a char * and returns void
+ *
+ * Return: void
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
