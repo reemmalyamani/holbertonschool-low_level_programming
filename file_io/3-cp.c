8a754e437fdb529ebbb2d5af16a967587f907789
reemmalyamani <alyamanireem@gmail.com>
2025-11-25 08:37:49 +0000

    fixing errors

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -29,7 +29,7 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", av[0]);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
