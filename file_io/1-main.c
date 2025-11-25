5c72c90bfc19c23bb340a87d708a73892d2fd86c
reemmalyamani <alyamanireem@gmail.com>
2025-11-25 08:27:42 +0000

    fixing betty styke

--- file_io/1-main.c
+++ file_io/1-main.c
@@ -3,20 +3,24 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the create_file function
+ * @ac: argument count
+ * @av: argument vector
  *
  * Return: Always 0.
  */
 int main(int ac, char **av)
 {
-    int res;
+	int res;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = create_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: %s filename text\n", av[0]);
+		exit(1);
+	}
+
+	res = create_file(av[1], av[2]);
+	printf("-> %i)\n", res);
+
+	return (0);
 }
