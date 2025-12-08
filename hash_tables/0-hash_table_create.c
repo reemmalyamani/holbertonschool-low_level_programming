4dfa1492ad7636b4eaddf34fd95f658a401698da
reemmalyamani <alyamanireem@gmail.com>
2025-12-08 14:36:08 +0000

    Add task 0: implement hash_table_create and header file

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,33 +2,32 @@
 
 /**
  * hash_table_create - Creates a hash table
- * @size: size of the array
+ * @size: Size of the array
  *
- * Return: pointer to the new hash table, or NULL on failure
+ * Return: Pointer to the new hash table, or NULL on failure
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table;
-    unsigned long int i;
+	hash_table_t *table;
+	unsigned long int i;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    table = malloc(sizeof(hash_table_t));
-    if (table == NULL)
-        return (NULL);
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
 
-    table->size = size;
+	table->size = size;
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
 
-    table->array = malloc(sizeof(hash_node_t *) * size);
-    if (table->array == NULL)
-    {
-        free(table);
-        return (NULL);
-    }
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
 
-    for (i = 0; i < size; i++)
-        table->array[i] = NULL;
-
-    return (table);
+	return (table);
 }
