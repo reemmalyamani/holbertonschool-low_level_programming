5e1b2b4c696d4746ef36e4442cfe9a2f6a2f6cbd
reemmalyamani <alyamanireem@gmail.com>
2025-12-08 14:33:47 +0000

    Add hash_table_create task and header

---
+++ hash_tables/0-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+int main(void)
+{
+    hash_table_t *ht;
+
+    ht = hash_table_create(1024);
+    if (!ht)
+        return (1);
+
+    printf("%p\n", (void *)ht);
+    return (0);
+}
